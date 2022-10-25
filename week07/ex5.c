76715869feb10bcccb6fad8da82c58b742525263
RoukayaZaki <roka.zaki2002@gmail.com>
2022-10-25 23:46:11 +0300

    Assignment 7

--- week07/ex5.c
+++ week07/ex5.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-	char **s = (char**)malloc(sizeof(char*));	// allocate memory for the pointer
+	char **s = (char**) malloc(sizeof(char*));	// allocate memory for the pointer
 	char foo[] = "Hello World";
 	*s = foo;
-	printf("s is %p\n", s);	 // would be meaningless to print *s, as it is the same thing as s[0] (next)
+	printf("s is %p\n", s);	 // s in fact is a pointer to array of chars (string).
 	s[0] = foo;
 	printf("s[0] is %s\n", s[0]);
 	
