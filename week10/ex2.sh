887ddfc617f192a572150cf241fb14b1ef48e958
RoukayaZaki <roka.zaki2002@gmail.com>
2022-11-15 01:23:28 +0300

    Assignment 10

--- week10/ex2.sh
+++ week10/ex2.sh
@@ -3,8 +3,10 @@ echo "Hello, Roukaya Mohammed!" > ../week01/file.txt
 link ../week01/file.txt _ex2.txt
 INUM=$(ls -i _ex2.txt | cut -d ' ' -f 1)
 find .. -inum "$INUM" > ex2.txt
-echo -e "Before removing _ex2.txt using link path:\n" >> ex2.txt
+echo -e "\nBefore removing _ex2.txt using link path:" >> ex2.txt
 ls >> ex2.txt
 find . -inum "$INUM" -exec rm {} \;
-echo -e "After removing _ex2.txt using link path:\n" >> ex2.txt
+echo -e "\nAfter removing _ex2.txt using link path:" >> ex2.txt
 ls >> ex2.txt
+
+#PS: file.txt would be found in week01 directory.
